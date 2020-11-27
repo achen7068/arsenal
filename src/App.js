a2ebff366d5d62a560c439a840590999eaa7d858
Alex <achen7068@protonmail.com>
2020-11-27 18:55:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Homepage from "./Component/Homepage/Homepage";
+function App() {
+  return (
+    <div className="App">
+        <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import styles from './Homepage.module.css';
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    Arsenal Solutions
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Homepage;
